4ff52fe72921739aa08f6d0de9014d7d4dba228e
GJhinger <gurshanjhinger22@gmail.com>
2022-02-26 18:29:21 -0800

    Navbar mostly finished, some stuff left for contact link to finish.

--- components/Nav/nav.jsx
+++ components/Nav/nav.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './nav.module.css'
+import logo from '../../assets/img/logo.svg'
 
 const Nav = () => {
     return (
-        <div>
-            Navbar n Shit'
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <Image src={logo} alt="Gurshan Jhinger"/>
+            </div>
+            <hr className={styles.break} />
+            <ul className={styles.list}>
+                <li className={styles.listItem}>
+                    <Link href='/resume.pdf'>
+                        <a target='_blank'>resume</a>
+                    </Link>
+                </li>
+                <li className={styles.listItem}>contact</li>
+            </ul>
         </div>
     )
 }
