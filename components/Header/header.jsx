80e29d9fd2f98372f5e4fb3d504097d0878a626b
GJhinger <gurshanjhinger22@gmail.com>
2022-03-17 20:34:52 -0700

    final

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
                 <div className={styles.introduction}>hello, my name is</div>
                 <h1 className={styles.name}> GURSHAN <span className={styles.lastName}>JHINGER</span> </h1>
                 <p className={styles.description}>
-                    I'm currently a fourth year <span className={styles.important}>Computer Science</span> student at <span className={styles.important}> Simon Fraser
-                    University</span> in Burnaby, British Columbia. I'm an aspiring Software Engineer who
+                    I&apos;m currently a fourth year <span className={styles.important}>Computer Science</span> student at <span className={styles.important}> Simon Fraser
+                    University</span> in Burnaby, British Columbia. I&apos;m an aspiring Software Engineer who
                     loves to design and develop tools for the web, you can check out some of my work
                     below.
                 </p>
